629ced7fcb4f2b6a226d15897ed22dbc65e83a8f
Michael Mello <michaelkcms@gmail.com>
2023-10-21 05:51:54 -0300

    Atualização

---
+++ Context/AgendaContext.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.Net.Http.Headers;
+using dotnet_API_learn.Entities
+
+namespace dotnet_API_learn.Context
+{
+    public class AgendaContext : DbContext
+    {
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options)
+        {
+
+        }
+        
+        public DbSet<Contato> Contatos{get; set;}
+    }
+}
